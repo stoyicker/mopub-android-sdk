cb3d0cd0aae51bac9f71998cb4844b39bdb637d1
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2016-01-15 16:32:10 +0100

    Make the example more clear

--- nullrootview/src/main/java/com/example/nullrootview/MainActivity.java
+++ nullrootview/src/main/java/com/example/nullrootview/MainActivity.java
@@ -2,41 +2,19 @@ package com.example.nullrootview;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
-import com.mopub.mobileads.MoPubInterstitial;
-import java.util.HashMap;
-import java.util.Map;
 
 public class MainActivity extends Activity {
 
-  private static final String INTERSTITIAL_ID = "YOUR_ID_WITH_MRAIDS_HERE";
-  private MoPubInterstitial mMoPubInterstitial;
-
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(new LinearLayout(this));
+    final ViewGroup viewGroup;
+    setContentView(viewGroup = new LinearLayout(this));
 
-    addInterstitial();
-    loadAd();
-    showAd();
-  }
-
-  private void addInterstitial() {
-    mMoPubInterstitial = new MoPubInterstitial(this, INTERSTITIAL_ID);
-    final Map<String, Object> extras = new HashMap<>();
-    DisplayMetrics metrics = new DisplayMetrics();
-    getWindowManager().getDefaultDisplay().getMetrics(metrics);
-    extras.put("screenWidth", String.valueOf(metrics.widthPixels));
-    extras.put("screenHeight", String.valueOf(metrics.heightPixels));
-    mMoPubInterstitial.setLocalExtras(extras);
-  }
-
-  private void loadAd() {
-    mMoPubInterstitial.load();
-  }
-
-  private void showAd() {
-    mMoPubInterstitial.show();
+    //See how it is not true, but is assumed to be always true by the SDK
+    Log.d("Assertion", "Does the set view get android.R.id.content as id? " + (viewGroup.getId()
+        == android.R.id.content));
   }
 }
